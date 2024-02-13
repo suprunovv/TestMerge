9c645734e5bc193963068cb2b838b14810f2aae9
Suprunov Anton <suprunovvanton666@gmail.com>
2024-02-13 19:42:01 +0300

    Сделал переход на второй экран

--- testAplication/testAplication/FirstViewController.swift
+++ testAplication/testAplication/FirstViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    let button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 300, width: 100, height: 100))
+        button.setTitle("secondVC", for: .normal)
+        button.backgroundColor = .blue
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(goToSecVC), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(button)
         view.backgroundColor = .green
+    }
+                         
+    @objc func goToSecVC() {
+        let secondVC = SecondVC()
+        present(secondVC, animated: true, completion: nil)
     }
 }
